02c65ec2a7febf68b90a41c8573e17b76b69302a
vinay-ryakala <vinayrakyala@gmail.com>
2021-01-26 20:18:11 +0530

    ADDED:minimum fare and test case for minimum fare

--- src/main/java/com/bribgelabz/InvoiceGenerator.java
+++ src/main/java/com/bribgelabz/InvoiceGenerator.java
@@ -4,9 +4,11 @@ public class InvoiceGenerator
 {
    public static final double MIN_COST_PER_KM = 10.0;
    public static final int COST_PER_TIME = 1;
+   public static final double MIN_FARE = 5.0;
 
    public double calculateFare(double distance, int time)
    {
-      return distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+      double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+      return Math.max(totalFare, MIN_FARE);
    }
 }
--- src/test/java/com/bribgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bribgelabz/InvoiceServiceTest.java
@@ -20,4 +20,11 @@ public class InvoiceServiceTest
       double fare = invoiceGenerator.calculateFare(distance, time);
       Assert.assertEquals(25, fare, 0.0);
    }
+   @Test
+   public void givenLessDistanceTimeReturnMinFare() {
+      double distance = 0.1;
+      int time = 1;
+      double fare = invoiceGenerator.calculateFare(distance, time);
+      Assert.assertEquals(5, fare, 0.0);
+   }
 }
